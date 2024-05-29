c0df754dc42ca239186e2c2d1aa2ec8724424eeb
AVillatoroG17 <avillatorog17@gmail.com>
2024-05-28 23:49:56 -0600

    "Nuevas funciones de peticiones"

---
+++ src/peticionUsuario.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class peticionUsuario {
+    Scanner in = new Scanner(System.in);
+
+    private String Doctor, Medico, Secretaria, Administrador;
+    private int opcion;
+
+    public void pedirUsuario(){
+
+        System.out.println("Con que usuario desea ingresar: ");
+        System.out.println("1. Doctor ");
+        System.out.println("2. Medico ");
+        System.out.println("3. Secretaria");
+        System.out.println("4. Administrador");
+        this.opcion = in.nextInt();
+
+        switch (opcion){
+
+            case 1:
+                System.out.println("Bienvenido a la clinica Ingenieria total");
+                System.out.println("Usted ha ingresado con el perfil Doctor");
+                this.Doctor = in.nextLine();
+                break;
+
+            case 2:
+                System.out.println("Bienvenido a la clinica Ingenieria total");
+                System.out.println("Usted ha ingresado con el perfil Medico");
+                this.Medico = in.nextLine();
+                break;
+
+            case 3:
+                System.out.println("Bienvenido a la clinica Ingenieria total");
+                System.out.println("Usted ha ingresado con el perfil Secretaria");
+                this.Secretaria = in.nextLine();
+                break;
+
+            case 4:
+                System.out.println("Bienvenido a la clinica Ingenieria total");
+                System.out.println("Usted ha ingresado con el perfil Administrador");
+                this.Administrador = in.nextLine();
+                break;
+
+        }
+
+    }
+
+}
