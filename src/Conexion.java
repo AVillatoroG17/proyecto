15c58ad5128b0cf66a21836ca275c18d54f9e739
AVillatoroG17 <avillatorog17@gmail.com>
2024-05-31 00:38:59 -0600

    Cambios en errores del ultimo commit

--- src/Conexion.java
+++ src/Conexion.java
@@ -7,11 +7,10 @@ public class Conexion {
 
     public Conexion() throws SQLException {
         try {
-<<<<<<< HEAD
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/clinica", "root", "SOMOSPROSSIUUU");
-=======
+
             connection = DriverManager.getConnection("", "ingresa el usuario //creo que es root", "//ingresa la contraseña que elegista al instalar el MySQL");
->>>>>>> 1fd53b1995c18006faca12bece30682607af5c54
+
         } catch (SQLException e) {
             e.printStackTrace();
             throw new SQLException("Error al conectar con la base de datos :( " + e.getMessage());
--- src/OpcionesPaciente.java
+++ src/OpcionesPaciente.java
@@ -6,21 +6,13 @@ import java.util.Scanner;
 
 public class OpcionesPaciente {
 
-    public void opcionePaciente(){
+    public void opcionePaciente() {
         System.out.println("¿ Que accion desea realizar ?");
         Scanner in = new Scanner(System.in);
         System.out.println("\n1. Agregar paciente");
         System.out.println("2. ver pacientes");
         int opcion = in.nextInt();
 
-        if(opcion == 1 ){
-            try{
-                Conexion conexion = new Conexion();
-                String sql = "INSERT INTO pacientes (nombre_paciente, apellido_paciente, DPI, sexo, tipo_de_sangre, edad, id_paciente, id_cita, id_medico, telefono) VALUES (?,?,?,?,?,?,?,?,?,?)";
-
-            }
-
-        }
 
         if (opcion == 1) {
             try {
@@ -100,8 +92,8 @@ public class OpcionesPaciente {
             }
         }
     }
-}
 
+}
 
 
 
